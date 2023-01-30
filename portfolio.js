a75aab6e24644605fab9a756d13d4a279037cfc2
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-01-30 16:08:54 +0100

    Add control for mobile menu toggles

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+function mobileMenuClicked(event) {
+  console.log(event);
+  document.getElementById("mobile-menu-icon").classList.toggle("expanded");
+  document.getElementById("mobile-menu-items").toggleAttribute("hidden");
+}
+
+document
+  .querySelector("#mobile-menu-icon")
+  .addEventListener("click", mobileMenuClicked);
+
+document.querySelectorAll("#mobile-menu-items li").forEach((item) => {
+  item.addEventListener("click", mobileMenuClicked);
+});
